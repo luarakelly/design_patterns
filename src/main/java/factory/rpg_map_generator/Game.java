145058f7b54d5fd80303e03de923aaa661f0fcad
luarakelly <lua.kellly@gmail.com>
2026-08-26 00:42:07 +0300

    feat: add WildernessMap creation to Game

--- src/main/java/factory/rpg_map_generator/Game.java
+++ src/main/java/factory/rpg_map_generator/Game.java
@@ -1,10 +1,21 @@
 package src.main.java.factory.rpg_map_generator;
 
+import java.util.Random;
+
 public class Game {
 
     public static Map createMap() {
-        System.out.println("City map:");
-        return new CityMap();
+        Random random = new Random();
+
+        int mapType = random.nextInt(2);
+
+        if (mapType == 1) {
+            System.out.println("City map");
+            return new CityMap();
+        } else {
+            System.out.println("wilderness map");
+            return new WildernessMap();
+        }
     }
 
     public static void main(String[] args) {
