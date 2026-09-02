a693a296d79e2e646eaa0fe46ac310f1824c6568
luarakelly <lua.kellly@gmail.com>
2026-09-02 08:39:03 +0300

    feat: defines methods for creating different user interface elements

---
+++ src/main/java/abstractfactory/ASCII_art/UIFactory.java
@@ -0,0 +1,9 @@
+package abstractfactory.ASCII_art;
+
+public abstract class UIFactory {
+    public abstract Button createButton(String text);
+
+    public abstract Checkbox createCheckbox(String text);
+
+    public abstract TextField createTextField(String text);
+}
