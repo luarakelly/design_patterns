ece49369baa5f013603e08ccf6d4bc4aea3870f6
luarakelly <lua.kellly@gmail.com>
2026-09-02 09:17:42 +0300

    feat: add xoncret factory for UI elements, for its corresponding products.

---
+++ src/main/java/abstractfactory/ASCII_art/BFactory.java
@@ -0,0 +1,19 @@
+package abstractfactory.ASCII_art;
+
+public class BFactory extends UIFactory {
+    @Override
+    public Button createButton(String text) {
+        return new ButtonB(text);
+    }
+
+    @Override
+    public Checkbox createCheckbox(String text) {
+        return new CheckboxB(text);
+    }
+
+    @Override
+    public TextField createTextField(String text) {
+        return new TextFieldB(text);
+    }
+
+}
